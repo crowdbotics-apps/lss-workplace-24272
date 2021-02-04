cd7684e85ae92c33eba4258e8e9bf084a12aab24
Crowdbotics <team@crowdbotics.com>
2021-02-04 10:08:20 +0000

    Update models

--- backend/lss_workplace_24272/wsgi.py
+++ backend/lss_workplace_24272/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lss_workplace_24272.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lss_workplace_24272.settings")
 
 application = get_wsgi_application()
